f9d907827efe8356f2404806d9db5b0595d8f020
cyberkat7217 <sreehari7217@gmail.com>
2023-08-09 02:34:57 +1000

    init an server with express
    -added html page
    -added two api logic in the backend
    -introduced require dependencies

---
+++ server.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let app = express();
+let port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', (req, res)=>{
+    res.render('index.html');
+
+});
+
+app.get('/addTwoNumbers',(req, res)=>{
+    //addTwoNumbers?number1=1&number2=2
+    let num1 = req.query.number1;
+    let num2 = req.query.number2;
+    //do math
+    let sum =parseInt(num1) + parseInt(num2);
+    // construct response, include: statusCode, Message, data
+    let obj = {statusCode:200, message:'success', data:sum}
+    //then send back
+    res.json(obj);
+})
+
+app.listen(port, ()=>{
+ console.log('Server Started 2');   
+});
